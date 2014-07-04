bcedd5047215568bbb0794538a02e4def40ac885
andrewmontague <andrew.m.montague@gmail.com>
2014-07-04 16:20:29 +1200

    ititial tests for JSON method finished and passing, 1 pending

--- spec/actions/processJSONSpec.js
+++ spec/actions/processJSONSpec.js
@@ -9,9 +9,15 @@ describe("Process JSON function", function(){
 	});
 
 	describe("After the processing of JSON data", function(){
+		
 		beforeEach(function(){
-			processJson();
+			processJson();	
 		});
+
+		afterEach(function(){
+			cityArray = [];
+		});
+		
 
 		it("cityArray will no longer be empy", function(){
 			expect(cityArray.length).not.toEqual(0);
@@ -19,6 +25,14 @@ describe("Process JSON function", function(){
 
 		it("the objects inside cityArray should be City objects", function(){
 			expect(cityArray[1] instanceof City).toBe(true);
+		});
+
+		it("A city object should have a name", function(){
+			expect(cityArray[0].name).not.toBe(null);
+		});
+
+		it("cityArray should have all the items from JSON in its array", function(){
+			expect(cityArray.length).toEqual(2);
 		});
 	});
 
