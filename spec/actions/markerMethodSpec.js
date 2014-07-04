c63afc244eec64cacc35a03d9a1c6370541ca1cd
andrewmontague <andrew.m.montague@gmail.com>
2014-07-04 16:45:45 +1200

    befre push

--- spec/actions/markerMethodSpec.js
+++ spec/actions/markerMethodSpec.js
@@ -2,4 +2,12 @@ describe("MarkerMethod", function(){
 	xit("makes new google.maps.Marker object",function(){
 
 	});
+
+	beforeEach(function(){
+		markerMethod();
+	});
+
+	xit("will have made maker objects", function(){
+		expect(return).not.toBe(null);
+	});
 });
