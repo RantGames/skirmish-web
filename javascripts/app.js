b2171ceb9a1891155a1ac52be15a7d4e8f8706e4
Nick Johnstone <ncwjohnstone@gmail.com>
2014-07-04 17:20:18 +1200

    Implemented SkirmishClient#login

--- javascripts/app.js
+++ javascripts/app.js
@@ -3,86 +3,34 @@
 
 "use strict";
 var SkirmishClient = (function () {
-
-    var $loginForm,
+    var endpoints,
         publicAttributes;
 
-    function login(e) {
-        e.preventDefault();
-        console.log('submitting login form');
-        var rawCredentials = $loginForm.serializeArray(),
-            credentials,
-            i,
-            cred;
+    endpoints = {
+        login: '/users/sign_in'
+    };
 
-        credentials = {
-            'user': {}
+    function login(email, password) {
+        var credentials = {
+            user: {
+                email: email,
+                password: password,
+                remember_me: '0'
+            }
         };
 
-        for (i = 0; i < rawCredentials.length; i += 1) {
-            cred = rawCredentials[i];
-
-            credentials.user[cred.name] = cred.value;
-        }
-
-        console.log(credentials);
-
-
         $.ajax({
-            type: 'POST',
-            url: 'http://localhost:3000/users/sign_in',
+            url: endpoints.login,
             data: JSON.stringify(credentials),
-            success: function () {
-                console.log('signed in');
-            },
-            contentType: "application/json",
-            dataType: 'json'
+            type: 'POST',
+            contentType: 'application/json',
+            dataType:  'json'
         });
-    }
-
-    function setupAjax() {
-        $.ajaxSetup({
-            headers: {
-                'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
-            }
-        });
-    }
-
-    function start() {
-        $loginForm = $('#login-form');
-        console.log('starting SkirmishClient');
-        $loginForm.on('submit', login);
-        setupAjax();
     }
 
     publicAttributes = {
-        start: start
+        login: login
     };
 
     return publicAttributes;
 }());
-
-$(function () {
-    console.log('js loaded');
-    SkirmishClient.start();
-});
-
-
-
-// http://css-tricks.com/snippets/jquery/serialize-form-to-json/
-$.fn.serializeObject = function () {
-    var o = {},
-        a = this.serializeArray();
-
-    $.each(a, function () {
-        if (o[this.name] !== undefined) {
-            if (!o[this.name].push) {
-                o[this.name] = [o[this.name]];
-            }
-            o[this.name].push(this.value || '');
-        } else {
-            o[this.name] = this.value || '';
-        }
-    });
-    return o;
-};
--- spec/appSpec.js
+++ spec/appSpec.js
@@ -1,2 +1,39 @@
-describe("App", function() {
+/*global describe, it, $, spyOn, expect, SkirmishClient, beforeEach */
+"use strict";
+
+describe("SkirmishClient", function () {
+    describe('logging in', function () {
+        beforeEach(function () {
+            spyOn($, 'ajax');
+
+            SkirmishClient.login('neo@matr.ix', 'swordfish');
+
+            this.requestArgs = $.ajax.calls.argsFor(0)[0];
+        });
+
+        it('is a post request', function () {
+            expect(this.requestArgs.type).toEqual('POST');
+        });
+
+        it('sends to the sign in url', function () {
+            expect(this.requestArgs.url).toEqual('/users/sign_in');
+        });
+
+        it('sends JSON content', function () {
+            expect(this.requestArgs.contentType).toEqual("application/json");
+            expect(this.requestArgs.dataType).toEqual("json");
+        });
+
+        it('sends the stringified credentials', function () {
+            var expected_credentials = {
+                user: {
+                    email: 'neo@matr.ix',
+                    password: 'swordfish',
+                    remember_me: '0'
+                }
+            };
+
+            expect(this.requestArgs.data).toEqual(JSON.stringify(expected_credentials));
+        });
+    });
 });
