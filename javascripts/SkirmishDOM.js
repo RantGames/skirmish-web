323a2ff7d1630bc3938050da5a56aeee7442dd39
Nick Johnstone <ncwjohnstone@gmail.com>
2014-07-05 16:19:41 +1200

    Implemented SkirmishDOM.getLoginCredentials

--- javascripts/SkirmishDOM.js
+++ javascripts/SkirmishDOM.js
@@ -4,13 +4,23 @@
 var SkirmishDOM = (function () {
     var $loginForm = $('#login-form');
 
+    function getLoginFormValue(name) {
+        //  TODO: fix ickyness here. Have to refer to SkirmishDOM.$loginForm to allow mocking.
+        //        Should break login form out into a module
+        return SkirmishDOM.$loginForm.find('input[name="' + name + '"]').val();
+    }
+
     function hideLoginForm() {
         $loginForm.hide();
     }
 
     function getLoginCredentials() {
-        throw "Not Implemented";
+        return {
+            email: getLoginFormValue('email'),
+            password: getLoginFormValue('password')
+        };
     }
+
     return {
         getLoginCredentials: getLoginCredentials,
         $loginForm: $loginForm,
