251c51fe96c5e2844da923eeab98d1dd75d62ef7
Nick Johnstone <ncwjohnstone@gmail.com>
2014-07-06 19:45:25 +1200

    Tidied up SkirmishMap

--- javascripts/SkirmishMap.js
+++ javascripts/SkirmishMap.js
@@ -1,38 +1,46 @@
+/*global $, jQuery, alert, document, SkirmishClient, SkirmishMap, google */
+"use strict";
+
 var SkirmishMap = (function () {
 
     var map;
     function initialize() {
         console.log("SkirmishMap initializing");
         var mapOptions;
+
         mapOptions = {
-          center: new google.maps.LatLng(39.8282, -98.5795),
-          zoom: 5
+            center: new google.maps.LatLng(39.8282, -98.5795),
+            zoom: 5
         };
         map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-    };
+    }
 
-    var displayCity = function(city){
+    function displayCity(city) {
         var iconBase = 'http://www.gravatar.com/avatar/';
-        new google.maps.Marker({
+
+        return new google.maps.Marker({
             position: new google.maps.LatLng(city.latLng[0], city.latLng[1]),
             map: map,
             title: city.name + ' ' + city.playerId,
             icon: iconBase + '205e460b479e2e5b48aec07710c08d50?s=50',
             playerId: city.playerId
         });
-    };
+    }
 
-    var displayCities = function(cities){
-        for(var i = 0; i < cities.length; i++){
-            var city = cities[i];
+    function displayCities(cities) {
+        var city,
+            i;
+
+        for (i = 0; i < cities.length; i += 1) {
+            city = cities[i];
             SkirmishMap.displayCity(city);
-        };
-    };
+        }
+    }
 
-    return  {
+    return {
         displayCity: displayCity,
         displayCities: displayCities,
         initialize: initialize
     };
 
-})();
+}());
