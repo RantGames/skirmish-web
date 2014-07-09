b961f28eac4ef0744fc7dad334b12c5635f943cd
Richard Wood <rwoodnz@gmail.com>
2014-07-09 19:06:55 +1200

    changed unit select to straight count

--- javascripts/SkirmishTroupMovement.js
+++ javascripts/SkirmishTroupMovement.js
@@ -6,93 +6,101 @@ var SkirmishTroupMovement = (function () {
 
   var publik = {};
   var clickCount = 0;
-  var lastCityClicked = {id:-1};
+  var previousCityClicked = {id:-1};
 
   publik.setDefaults = function() {
     clickCount = 0
-    lastCityClicked = {id:-1};
+    previousCityClicked = {id:-1};
   }
 
-  publik.clickCount = function () { clickCount};
+  publik.clickCount = function () { clickCount} ;
   publik.setClickCount = function (value) {clickCount = value}
 
   publik.clickHandler = function (city) {
 
-    if (publik.validMoveClick(lastCityClicked, city)) {
-      var rawMove = publik.collateRawMove(lastCityClicked, city)
+    if (publik.validMoveClick(previousCityClicked, city)) {
+      var rawMove = publik.collateRawMove(previousCityClicked, city)
       SkirmishApp.sendMove(rawMove);
     }
 
-    publik.clickCountUpdater(lastCityClicked, city);
+    publik.clickCountUpdater(previousCityClicked, city);
 
     if (myCity(city)) {
-      SkirmishDOM.flash(clickCount*20 +'% of units selected in '+city.name)
+      var plurUnit = clickCount == 1 ? 'unit' : 'units'
+      SkirmishDOM.flash(clickCount+' '+plurUnit+' selected in '+city.name)
     }
 
-    lastCityClicked = city;
+    previousCityClicked = city;
 
   };
 
-  publik.collateRawMove = function(lastCityClicked, city) {
+  publik.collateRawMove = function(previousCityClicked, city) {
 
-    var moveType = isAttack(lastCityClicked, city) ? 'attack' : 'move'
+    var moveType = isAttack(previousCityClicked, city) ? 'attack' : 'move'
 
     var rawMove = {
       moveType: moveType,
-      originId: lastCityClicked.id,
+      originId: previousCityClicked.id,
       targetId: city.id,
-      unitCount: publik.calculateUnits(lastCityClicked,clickCount)
+      unitCount: publik.calculateUnits(previousCityClicked,clickCount)
     };
 
     return rawMove
   }
 
-  publik.calculateUnits = function(lastCityClicked,clickCount) {
-    var units = lastCityClicked.units.length;
-    if (clickCount <= 5) { units = Math.ceil(units*clickCount/5) };
+  publik.calculateUnits = function(previousCityClicked,clickCount) {
+    var units = previousCityClicked.units.length;
+    units = clickCount
+    // if (clickCount <= 5) { units = Math.ceil(units*clickCount/5) };
     return units
   };
 
-  publik.validMoveClick = function(lastCityClicked, city) {
+  publik.validMoveClick = function(previousCityClicked, city) {
     return (clickCount > 0 &&
-      lastCityClicked != null &&
-      !sameCity(lastCityClicked, city) &&
-      SkirmishGeo.checkRange(lastCityClicked, city)
+      previousCityClicked != null &&
+      !sameCity(previousCityClicked, city) &&
+      SkirmishGeo.checkRange(previousCityClicked, city)
       );
   };
 
-  publik.clickCountUpdater = function (lastCityClicked, city) {
-
-    if(myCity(city) && sameCity(lastCityClicked, city)) {
-      clickCount = (clickCount >= 5 ? 5 : clickCount + 1)
+  publik.clickCountUpdater = function (previousCityClicked, city) {
+    var max = maxClickCount(previousCityClicked, city)
+    if(myCity(city) && sameCity(previousCityClicked, city)) {
+      clickCount = (clickCount >= max ? max : clickCount + 1)
     } else {
-      clickCount = (myCity(city) ? 1 : 0)
+      if (myCity(city) && max > 0) {clickCount = 1}
+      else {clickCount = 0}
     };
+    console.log(clickCount)
     return clickCount
   }
 
-  publik.setClickCount = function (value) {
-    clickCount = value;
+  var maxClickCount = function (previousCityClicked, city) {
+    if (previousCityClicked.id == -1) {
+      return city.units.length;
+    } else {
+      return previousCityClicked.units.length;
+    }
   }
 
-  publik.lastCityClicked = function () {
-    return lastCityClicked;
+  publik.previousCityClicked = function () {
+    return previousCityClicked;
   }
 
-  publik.setLastCityClicked = function (value) {
-    lastCityClicked = value;
+  publik.setPreviousCityClicked = function (value) {
+    previousCityClicked = value;
   }
 
   var myCity = function(city) {
     return (city.playerId == SkirmishGameState.getCurrentPlayerId())
   }
 
-  var sameCity = function(lastCityClicked, city) {
-    return (lastCityClicked.id == city.id);
+  var sameCity = function(previousCityClicked, city) {
+    return (previousCityClicked.id == city.id);
   };
 
-  var isAttack = function(lastCityClicked, city) {
-    return lastCityClicked.playerId != city.playerId
+  var isAttack = function(previousCityClicked, city) {
+    return previousCityClicked.playerId != city.playerId
   };
 
 
